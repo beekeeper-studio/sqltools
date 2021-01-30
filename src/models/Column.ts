41ed13e2a67ee007fe4c3cea2ffc507f41a74d0b
nicollg <nicoll.g4@gmail.com>
2021-01-30 00:17:52 -0600

    Colums done

--- src/models/Column.ts
+++ src/models/Column.ts
@@ -41,24 +41,23 @@ export const ColumnTypeOptions: ColumnOption[] = [
   { name: 'integer', description: 'Adds an integer column', hasUnsigned: true},
   { name: 'biginteger', description: 'Adds a bigint column, bigint data is returned as string', hasUnsigned: true},
   { name: 'enum', requiresValues: true, description: 'Adds a enum column, unchecked varchar(255) on Amazon Redshift', hasUnsigned: true},
-
-
+  { name: 'text', description: 'Adds a text column', hasUnsigned: true},
+  { name: 'string', description: 'Adds a string column, default length 255', hasUnsigned: true},
+  { name: 'Float',description: 'Adds a float column, optional precision (defaults to 8) and scale (defaults to 2)', hasUnsigned: true},
+  { name: 'Decimal',  description: 'Adds a decimal column, optional precision (defaults to 8) and scale (defaults to 2). Choosing NULL as precision creates a decimal column that can store numbers of any precision and scale. (Only supported for Oracle, SQLite, Postgres)', hasUnsigned: true},
+  { name: 'Boolean' , description: 'Adds a boolean', hasUnsigned: true},
+  { name: 'Date', description: 'Adds a date column', hasUnsigned: true},
+  { name: 'DateTime', description: 'Adds a datetime column, suppoorted by PostgreSQL creating a column with timezone', hasUnsigned: true},
+  { name: 'Time', description: 'Adds a time column, optional precision for SQL (not supported on Amazon Redshift)', hasUnsigned: true},
+  { name: 'TimeStamp', description: 'Adds a timeStamp column', hasUnsigned: true},
+  { name: 'Binary', description: 'Adds a binary column, with optional length argument for MySQL', hasUnsigned: true},
+  { name: 'Json', description: 'Adds a json column, it uses the built-in json type in PostgreSQL, MySQL and SQLite', hasUnsigned: true},
+  { name: 'Jsonb', description: 'Adds a jsonb column, it uses native jsonb', hasUnsigned: true},
+  { name: 'Uuid' , description: 'Adds an uuid column, it uses built-in uuid type in PostgreSQL and char(36) in other databases', hasUnsigned: true},
+  { name: 'SpecificType' , description: 'Creates your own column type', hasUnsigned: true},
 ]
-  // text: { description: 'Adds a text column', hasUnsigned: true},
-  // string: { description: 'Adds a string column, default length 255', hasUnsigned: true},
-  // [ColumnType.Float, { description: 'Adds a float column, optional precision (defaults to 8) and scale (defaults to 2)', hasUnsigned: true}],
-  // [ColumnType.Decimal, { description: 'Adds a decimal column, optional precision (defaults to 8) and scale (defaults to 2). Choosing NULL as precision creates a decimal column that can store numbers of any precision and scale. (Only supported for Oracle, SQLite, Postgres)', hasUnsigned: true}],
-  // [ColumnType.Boolean, { description: 'Adds a boolean', hasUnsigned: true}],
-  // [ColumnType.Date, { description: 'Adds a date column', hasUnsigned: true}],
-  // [ColumnType.DateTime, { description: 'Adds a datetime column, suppoorted by PostgreSQL creating a column with timezone', hasUnsigned: true}],
-  // [ColumnType.Time, { description: 'Adds a time column, optional precision for SQL (not supported on Amazon Redshift)', hasUnsigned: true}],
-  // [ColumnType.TimeStamp, { description: 'Adds a timeStamp column', hasUnsigned: true}],
-  // [ColumnType.Binary, { description: 'Adds a binary column, with optional length argument for MySQL', hasUnsigned: true}],
-  // [ColumnType.Json, { description: 'Adds a json column, it uses the built-in json type in PostgreSQL, MySQL and SQLite', hasUnsigned: true}],
-  // [ColumnType.Jsonb, { description: 'Adds a jsonb column, it uses native jsonb', hasUnsigned: true}],
-  // [ColumnType.Uuid, { description: 'Adds an uuid column, it uses built-in uuid type in PostgreSQL and char(36) in other databases', hasUnsigned: true}],
-  // [ColumnType.SpecificType, { description: 'Creates your own column type', hasUnsigned: true}],
-// }
+  
+
 
 
 
